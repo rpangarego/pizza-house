441be125efb5f4893500b15b039c36066dcb2466
Ronaldo Pangarego <63718825+rpangarego@users.noreply.github.com>
2020-09-22 11:17:09 +0700

    Update script.js
    add preloader functionality and update nav menu link function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-// SimpleLightbox
-var $gallery = new SimpleLightbox(".gallery a", {});
-// SmoothScroll
-var scroll = new SmoothScroll('a[href*="#"]');
+//Selector
+const preloader = document.getElementsByClassName("preloader")[0];
+const navAnchor = document.querySelectorAll(".nav-item a");
+const navList = document.querySelector(".nav-list");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+
+// Preloader Time-out
+setTimeout(() => {
+  preloader.style.display = "none";
+}, 5000);
 
 // Open-Close Menu
 document.addEventListener("DOMContentLoaded", () => {
-  const navList = document.querySelector(".nav-list");
-  const openMenu = document.querySelector(".open-menu");
-  const closeMenu = document.querySelector(".close-menu");
+  preloader.style.display = "none";
 
   openMenu.addEventListener("click", () => {
     navList.classList.add("active");
-    openMenu.style.display = "none";
   });
 
   closeMenu.addEventListener("click", () => {
     navList.classList.remove("active");
-    openMenu.style.display = "block";
   });
 });
+
+// Nav anchor
+navAnchor.forEach((menuLink) => {
+  menuLink.addEventListener("click", () => {
+    if (navList.classList.contains("active")) {
+      navList.classList.remove("active");
+    }
+  });
+});
+
+// SimpleLightbox
+var $gallery = new SimpleLightbox(".gallery a", {});
+// SmoothScroll
+var scroll = new SmoothScroll('a[href*="#"]');
